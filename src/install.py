72c41ec332bec58246165a71cbf2bf42a36c2cd4
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2023-02-20 19:26:37 +0100

    General update

--- src/install.py
+++ src/install.py
@@ -2,7 +2,9 @@
 
 import colorama
 import os
+import pathlib
 import subprocess
+from pathlib import Path
 from subprocess import Popen
 
 # endregion
@@ -10,6 +12,7 @@ from subprocess import Popen
 # region Variables
 
 source_directory: str = str(os.path.dirname(__file__))
+
 graphite_install_command: list[str] = [
     f"{source_directory}/submodules/Graphite-gtk-theme/install.sh",
     "--color",
@@ -43,19 +46,26 @@ nordzy_install_command: list[str] = [
     "--color",
     "--dark",
 ]
-
+nordzy_cursors_install_command: str = (
+    f"{source_directory}/Nordzy-cursors/install.sh/",
+)
 install_commands: list[list] = [
     graphite_install_command,
     graphite_install_command_gdm,
     graphite_install_command_libadwaita,
+    nordzy_install_command,
+    nordzy_cursors_install_command,
 ]
 
 # endregion
 
 
 def main():
-    for command in install_commands:
-        subprocess.run(command)
+    print(Path(source_directory).parent.absolute())
+
+
+    # for command in install_commands:
+    #     subprocess.run(command)
 
 
 if __name__ == "__main__":
